3265438c93dfb6aa6f9e0c58a3042b8dc37098a4
Melissa Bruno <brunomr@miamioh.edu>
2017-03-04 20:28:25 -0500

    Add files via upload

---
+++ Midterm1.ino
@@ -0,0 +1,81 @@
+/*
+ * Arduino program that uses the time a photoresistor has been
+ * exposed to create a string of 1's and 0's that is converted
+ * to morse code and printed to an LCD.
+ *
+ * Created February 2017
+ * By Melissa Bruno
+ *
+ * http://url/of/online/tutorial.cc
+ *
+ */
+
+#include "LiquidCrystal.h"
+#include "Control.h"
+#include "Morse.h"
+
+ControlClass control = ControlClass();
+MClass morse = MClass();
+
+int sensorPin, sensorValue;
+double startTime, endTime, timer, timeInSeconds;
+boolean flag;
+int switchState;
+boolean backFlag;
+
+
+void setup(){
+  control.SETUP();
+  Serial.begin(9600);
+  sensorPin = A0;
+  flag = false;
+  backFlag = false;
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);                     //Photoresistor value
+  switchState = digitalRead(8);                            //Back button state
+
+  /*
+   * Measuring the time that the photoresistor is exposed
+   */
+  if(sensorValue > 300 && flag == false){
+    startTime = millis();
+    flag = true;
+  }
+  if(sensorValue < 300 && flag == true){ 
+    endTime = millis();
+    timer = endTime - startTime;
+    timeInSeconds = timer/1000;
+    flag = false;
+
+    if(timeInSeconds < 0.3) {                             //If time is < 0.3s, a "0" is added to a String
+      morse.dot();
+    }
+    if(timeInSeconds > 0.3 && timeInSeconds < 1.0){       //If time is > 0.3s and < 1s, a "1" is added to the String
+      morse.dash();
+    }
+    if(timeInSeconds > 1.0){                              //If time is > 1s, morse code letter is printed to LCD
+      control.printLetter(morse.toMorseCode());
+      morse.clearLetter();
+
+      if(control.getCursorPosition() < 15){
+        control.cursorForward();
+      }
+      else if(control.getCursorPosition() == 15){         //Moving cursor to next row if it has reached the end of current row
+        control.incrementRow();
+      }
+    }
+  }
+
+  /*
+   * Backspace
+   */
+  if(switchState == HIGH){
+    backFlag = true;
+    delay(500);
+    control.backspace();
+    backFlag = false;
+  }
+} //end loop
---
+++ Morse.cpp
@@ -0,0 +1,151 @@
+/*
+ * Library for creating morse code
+ */
+
+#include "Arduino.h"
+#include "Morse.h"
+
+String letter;
+
+MClass::MClass(){
+}
+
+/*
+ * Adds a 0 to String letter
+ */
+void MClass::dot(){
+  letter += "0";
+}
+
+/*
+ * Adds a 1 to String letter
+ */
+void MClass::dash(){
+  letter += "1";
+}
+
+/*
+ * Removes all entries from String letter
+ */
+void MClass::clearLetter(){
+  letter = "";
+}
+
+/*
+ * Returns alphabetic letter that correlates
+ * to morse code value in String letter
+ */
+String MClass::toMorseCode(){
+  if(letter == "01"){
+    return("A");
+  }
+  if(letter == "1000"){
+    return("B");
+  }
+  if(letter == "1010"){
+    return("C");
+  }
+  if(letter == "100"){
+    return("D");
+  }
+  if(letter == "0"){
+    return("E");
+  }
+    if(letter == "0010"){
+    return("F");
+  }
+  if(letter == "110"){
+    return("G");
+  }
+  if(letter == "0000"){
+    return("H");
+  }
+  if(letter == "00"){
+    return("I");
+  }
+  if(letter == "0111"){
+    return("J");
+  }
+    if(letter == "101"){
+    return("K");
+  }
+  if(letter == "0100"){
+    return("L");
+  }
+  if(letter == "11"){
+    return("M");
+  }
+  if(letter == "10"){
+    return("N");
+  }
+  if(letter == "111"){
+    return("O");
+  }
+    if(letter == "0110"){
+    return("P");
+  }
+  if(letter == "1101"){
+    return("Q");
+  }
+  if(letter == "010"){
+    return("R");
+  }
+  if(letter == "000"){
+    return("S");
+  }
+  if(letter == "1"){
+    return("T");
+  }
+  if(letter == "001"){
+    return("U");
+  }
+  if(letter == "0001"){
+    return("V");
+  }
+  if(letter == "011"){
+    return("W");
+  }
+  if(letter == "1001"){
+    return("X");
+  }
+  if(letter == "1011"){
+    return("Y");
+  }
+  if(letter == "1100"){
+    return("Z");
+  }
+  if(letter == "11111"){
+    return("0");
+  }
+  if(letter == "01111"){
+    return("1");
+  }
+  if(letter == "00111"){
+    return("2");
+  }
+  if(letter == "00011"){
+    return("3");
+  }
+  if(letter == "00001"){
+    return("4");
+  }
+  if(letter == "00000"){
+    return("5");
+  }
+  if(letter == "10000"){
+    return("6");
+  }
+  if(letter == "11000"){
+    return("7");
+  }
+  if(letter == "11100"){
+    return("8");
+  }
+  if(letter == "11110"){
+    return("9");
+  }
+  if(letter ==  ""){
+    return(" ");
+  }
+}
+
